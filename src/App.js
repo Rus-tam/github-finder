e036c7dc9c4fc5b9bf516016938b7d7cb470d892
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2022-02-18 00:25:54 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Roter, Route, Routes } from 'react-router-dom';
+import Navbar from './component/layout/Navbar';
+import Footer from './component/layout/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <Roter>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Roter>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from '../component/users/UserResults';
+
+function Home() {
+  return (
+    <div>
+      {/* SEARCH COMPONENT */}
+      <UserResults />
+    </div>
+  );
+}
+
+export default Home;
