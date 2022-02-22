39c20b5270136e01ed3e843a66ec75064fe135c1
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2022-02-22 23:09:10 +0500

    reducer

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ import Footer from './component/layout/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
+import { GithubProvider } from './component/context/github/GithubContext';
 
 function App() {
   return (
-    <Roter>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/notfound" element={<NotFound />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Roter>
+    <GithubProvider>
+      <Roter>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notfound" element={<NotFound />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Roter>
+    </GithubProvider>
   );
 }
 
